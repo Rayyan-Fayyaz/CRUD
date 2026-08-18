1540782e014d29205eb5a3f4ae940891d1f8a1ed
Rayyan-Fayyaz <rayyanfayyaz24@gmail.com>
2026-08-18 22:35:46 +0500

    fixing bugs again

--- catalogue-webui/src/services/BookService.js
+++ catalogue-webui/src/services/BookService.js
@@ -2,20 +2,21 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8080/api/books";
 
-class BookService{
-    getAllBooks(){
+class BookService {
+
+    getAllBooks() {
         return axios.get(`${API_BASE_URL}/getAllBooks`);
     }
 
-    addBook(Book){                              // capital "Book" (the parameter)
-        return axios.post(`${API_BASE_URL}/Book`, book)   // lowercase "book" (used here)
+    addBook(book) {
+        return axios.post(`${API_BASE_URL}/addBook`, book);
     }
 
-    updateBooks(id, book){
-        return axios.post(`${API_BASE_URL}/updateBook/${id}`,book);
+    updateBook(id, book) {
+        return axios.put(`${API_BASE_URL}/updateBook/${id}`, book);
     }
 
-    deleteBook(id){
+    deleteBook(id) {
         return axios.delete(`${API_BASE_URL}/deleteBook/${id}`);
     }
 }
